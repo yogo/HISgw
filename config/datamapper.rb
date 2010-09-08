8f478d420c56f80fec29e23180cf52f2689e62c1
irjudson <irjudson@gmail.com>
2010-09-08 13:28:46 -0600

    fix up ignores so that datamapper.rb is in the git repo but the database.yml file isn't.

---
+++ config/datamapper.rb
@@ -0,0 +1,15 @@
+#
+# DataMapper Configuration of the three HIS Servers
+#
+configure :development do
+  databases = YAML.load(File.read(File.join(Dir.pwd, 'config', 'database.yml')))
+
+  # The production HIS Server
+  DataMapper.setup(:default, databases['default'])
+
+  # The AG HIS Server Brian McGlynn is using
+  DataMapper.setup(:tanager, databases['tanager'])
+
+  # The prototype HIS Server we'll remove soon
+  DataMapper.setup(:ravage, databases['ravage'])
+end
