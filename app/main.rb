8befdc1a24c36d1b0a18591c52f3dccde9a884c6
Sean Cleveland <sean.b.cleveland@gmail.com>
2010-10-20 15:17:45 -0600

    added xml header

--- app/main.rb
+++ app/main.rb
@@ -14,8 +14,8 @@ class HISGateway < Sinatra::Base
     end
 
     if format == "xml"
-      puts data.to_xml
-      return data.to_xml
+      
+      return '<?xml version="1.0" encoding="UTF-8"?>' +data.to_xml
     elsif format == "yaml"
       return data.to_yaml
     elsif format == "csv"
