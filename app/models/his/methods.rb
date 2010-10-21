6b46854601fc2ea020347849822944eebf64b962
Sean Cleveland <sean.b.cleveland@gmail.com>
2010-10-21 17:40:19 -0600

    Fixed a problem with the "Method" model since Method is a reserved word it was blowing up.

--- app/models/his/methods.rb
+++
@@ -1,7 +0,0 @@
-class Method
-  include DataMapper::Resource
-  storage_names[:default] = "Methods"
-
-  property :id, Serial, :required => true, :key => true, :field => "MethodID"
-  property :method_description, String, :required => true, :field => "MethodDescription"
-end
