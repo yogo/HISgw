ee4091dcb48c6a17f500884d1937c02ff3d887c8
Sean Cleveland <sean.b.cleveland@gmail.com>
2010-10-21 12:22:59 -0600

    Checking REXML XMLDecl command

--- app/main.rb
+++ app/main.rb
@@ -17,7 +17,7 @@ class HISGateway < Sinatra::Base
       content_type :xml
       xml_doc = data.to_xml_document
       xml_doc << REXML::XMLDecl.default
-      
+      putd xml_doc.to_s
       return xml_doc.to_s
     elsif format == "yaml"
       content_type :yaml
@@ -38,7 +38,7 @@ class HISGateway < Sinatra::Base
       content_type :xml
       xml_doc = data.to_xml_document
       xml_doc << REXML::XMLDecl.default
-      
+      puts xml_doc.to_s
       return xml_doc.to_s
     elsif types.include?('application/x-yaml') || types.include?('text/yaml')
       content_type :yaml
