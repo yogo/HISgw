2481e7145b617442381ec18b07980bbbee27371f
Robbie Lamb <robbie.lamb@gmail.com>
2011-04-06 10:07:35 -0600

    Configuring datamapper logging.

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -1,15 +1,22 @@
 #
 # DataMapper Configuration of the three HIS Servers
 #
-#configure :development do
-  databases = YAML.load(File.read(File.join(Dir.pwd, 'config', 'database.yml')))
+configure :development do
+  DataMapper::Logger.new(STDOUT, :debug)
+end
 
-  # The production HIS Server
-  DataMapper.setup(:default, databases['default'])
+configure :production do
+  DataMapper::Logger.new(STDOUT, :info)
+end
 
-  # The AG HIS Server Brian McGlynn is using
-  DataMapper.setup(:tanager, databases['tanager'])
+databases = YAML.load(File.read(File.join(Dir.pwd, 'config', 'database.yml')))
 
-  # The prototype HIS Server we'll remove soon
-  DataMapper.setup(:ravage, databases['ravage'])
-#end
+# The production HIS Server
+DataMapper.setup(:default, databases['default'])
+
+# The AG HIS Server Brian McGlynn is using
+DataMapper.setup(:tanager, databases['tanager'])
+
+# The prototype HIS Server we'll remove soon
+DataMapper.setup(:ravage, databases['ravage'])
+
